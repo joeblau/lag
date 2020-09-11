5d68c32713138979c8d41f2e5468289bfc16e0a6
Joe Blau <im@joeblau.com>
2020-09-11 12:25:52 -0700

    Initial Commit

---
+++ Latency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Latency
+//
+//  Created by Joe Blau on 9/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Latency/LatencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatencyApp.swift
+//  Latency
+//
+//  Created by Joe Blau on 9/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LatencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
